0f048d2189b90a7e7ef2ff791c106108862cb5b0
bobbyfromgeminus <info.geminus@gmail.com>
2020-12-12 20:32:16 +0100

    feat: todo project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Változók rögzítése
 const todoInput = document.querySelector('.todo-input');
 const addTodoButton = document.querySelector('.add-todo');
 const todoContainer = document.querySelector('.todo-container');
@@ -7,77 +8,91 @@ const completedContainer = document.querySelector('.completed-container');
 const todoCounterHolder = document.querySelector('.counter');
 const showHideButton = document.querySelector('.show-hide');
 const clearAllButton = document.querySelector('.clear-all');
-
+const currentDateHolder = document.querySelector('.actual-date');
 let todoCounter = 0;
 let storageId = 1;
 
+// Aktuális dátum
+const currentDate = () => {
+    const curDate = new Date();
+    const curY = curDate.getFullYear();
+    let curM = curDate.getMonth() + 1;
+    if (curM < 10) curM = `0${curM}`;
+    let curD = curDate.getDate();
+    if (curD < 10) curD = `0${curD}`;
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayName = days[curDate.getDay()];
+    currentDateHolder.innerHTML = `${dayName}<br>${curM}-${curD}-${curY}`;
+}
+currentDate();
 
+// localStorage kezelő objektum
+const storageHandler = {
+    set: (key, value) => localStorage.setItem(key, value),
+    get: key => localStorage.getItem(key),
+    reset: key => localStorage.removeItem(key),
+};
+
+// Számláló újraindítása
 const counterReset = () => {
     todoCounter = 0; 
     todoCounterHolder.textContent = todoCounter;
 }
 
+// Számláló módosítása
 const counterUpdater = (direction) => {
     if (direction) todoCounter += 1;
     else todoCounter -= 1; 
     todoCounterHolder.textContent = todoCounter;
 }
 
-
+// Teendő törlése
 const deleteTodo = (id) => {
     document.querySelector(`[data-id="${id}"]`).parentElement.remove();
-    localStorage.removeItem(id);
+    storageHandler.reset(id);
     counterUpdater(false);
 }
 
+// Teendő áthelyezésre
 const todoCompleted = (id) => {
-    let valueString = localStorage.getItem(id);
+    let valueString = storageHandler.get(id);
     valueString = valueString.replace('"state":1', '"state":2');
-    localStorage.setItem(id, valueString);
+    storageHandler.set(id, valueString);
     const targetCheckbox = document.querySelector(`[data-setid="${id}"]`);
     const targetTodo = targetCheckbox.parentElement;
     targetCheckbox.disabled = true;
     targetTodo.remove();
     completedContainer.insertBefore(targetTodo, completedContainer.firstChild);
     counterUpdater(false);
-
 }
 
-
+// Teendőn belüli eseménykezelők
 const addDeleteEventListener = (id) => document.querySelector(`[data-id="${id}"]`).addEventListener('click', () => deleteTodo(id));
-
 const addSetEventListener = (id) => document.querySelector(`[data-setid="${id}"]`).addEventListener('click', () => todoCompleted(id));
 
-
+// Teendő hozzáadása localStorage-ből
 const createTodo = (text, id, state, datetime) => {
     let isChecked = '';
     let parentContainer = todoContainer;
     const todoItem = document.createElement('div');
-    todoItem.classList.add('todo-item');
+    todoItem.classList.add('todo-item', 'slide-in');
     todoItem.setAttribute('title', datetime);
     if (parseInt(state) === 2) {
         parentContainer = completedContainer;
         isChecked = 'checked disabled';
-    } else {
-        counterUpdater(true);
-    }
-    todoItem.innerHTML = `<input type="checkbox" ${isChecked} name="set-completed" class="set-completed" data-setid="${id}"> ${text} <button class="delete-button" data-id="${id}">X</button>`;
+    } else counterUpdater(true);
+    todoItem.innerHTML = `  <input type="checkbox" ${isChecked} name="set-completed" class="set-completed" data-setid="${id}">
+                            ${text}
+                            <button class="delete-button" data-id="${id}"><i class="fa fa-trash"></i></button>`;
     parentContainer.insertBefore(todoItem, parentContainer.firstChild);
 }
 
+// Új teendő hozzáadása
 const addTodo = () => {
     if (todoInput.value) {
         const currentDate = new Date();
         createTodo(todoInput.value, storageId, 1, currentDate);
-
-        localStorage.setItem(storageId.toString(), JSON.stringify(
-            {
-                todo: todoInput.value,
-                state: 1,   // 1, ha aktív a feladat | 2, ha inaktív
-                createDate: currentDate, 
-            }
-        )
-        );
+        storageHandler.set(storageId.toString(), JSON.stringify({todo: todoInput.value, state: 1, createDate: currentDate, }));
         addDeleteEventListener(storageId);
         addSetEventListener(storageId);
         todoInput.value = '';
@@ -85,15 +100,19 @@ const addTodo = () => {
     }
 }
 
-// Az oldal betöltésekor átfutjuk a localStorage-et
-Object.keys(localStorage).forEach((key) => {
-    const obj = JSON.parse(localStorage.getItem(key));
-    createTodo(obj.todo, key, obj.state);
-    addDeleteEventListener(key);
-    addSetEventListener(key);
-    if(parseInt(key) >= storageId) storageId = parseInt(key) + 1;
-});
+// Teendő lista felépítése locasStorage-ből az oldal betöltésekor
+const buildTodoList = () => {
+    Object.keys(localStorage).forEach((key) => {
+        const obj = JSON.parse(storageHandler.get(key));
+        createTodo(obj.todo, key, obj.state);
+        addDeleteEventListener(key);
+        addSetEventListener(key);
+        if(parseInt(key) >= storageId) storageId = parseInt(key) + 1;
+    });
+}
+buildTodoList();
 
+// Elvégzett teendők mutatása/elrejtése
 const setShowHide = () => {
     const btnContent = showHideButton.textContent;
     if (btnContent == 'Show Complete') {
@@ -105,23 +124,22 @@ const setShowHide = () => {
     }
 }
 
+// Összes el nem végzett teendő törlése
 const clearAll = () => {
     Object.keys(localStorage).forEach((key) => {
-        const obj = JSON.parse(localStorage.getItem(key));
+        const obj = JSON.parse(storageHandler.get(key));
         if (parseInt(obj.state) === 1) {
-            localStorage.removeItem(key);
+            storageHandler.reset(key);
             document.querySelector(`[data-setid="${key}"]`).parentElement.remove();
             counterReset();
         }
     });
 }
 
+// Eseménykezelők
 const addTodoClickListener = () => addTodoButton.addEventListener('click', addTodo);
-
 const addShowHideClickListener = () => showHideButton.addEventListener('click', setShowHide);
-
 const clearAllClickListener = () => clearAllButton.addEventListener('click', clearAll);
-
 addTodoClickListener();
 addShowHideClickListener();
 clearAllClickListener();
